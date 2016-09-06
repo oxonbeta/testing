13db23cf7e8e2abb3cd878372086d51ff9c7808c
Ángel <noreply>
2016-09-06 17:22:45 +0000

    Add module uno

---
+++ uno.js
@@ -0,0 +1,15 @@
+/**
+ * @module //uno
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Uno
+ * @extends Montage
+ */
+exports.Uno = Montage.specialize(/** @lends Uno# */ {
+    constructor: {
+        value: function Uno() {
+            this.super();
+        }
+    }
+});
